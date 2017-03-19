fe17afe55684643116dd5bbacbbb25cb2361dcd7
SimonSun <simon.sun.dev@hotmail.com>
2017-03-19 22:40:06 +0800

    ApiserverApplication类增加关于zuul和eurekad额注解

--- src/main/java/com/simon/ApiserverApplication.java
+++ src/main/java/com/simon/ApiserverApplication.java
@@ -2,8 +2,12 @@ package com.simon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
+@EnableDiscoveryClient
 public class ApiserverApplication {
 
 	public static void main(String[] args) {
