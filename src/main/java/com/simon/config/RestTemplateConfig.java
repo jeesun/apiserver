902ad8c242f5ff466fb0eb8f3e9499294510984c
SimonSun <simon.sun.dev@hotmail.com>
2017-04-13 23:46:12 +0800

    允许跨域请求，以解决RestTemplate请求的服务不在同一台服务器，造成Connection refused的问题。

--- src/main/java/com/simon/config/RestTemplateConfig.java
+++ src/main/java/com/simon/config/RestTemplateConfig.java
@@ -3,6 +3,8 @@ package com.simon.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -12,7 +14,15 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);//ms
+        factory.setConnectTimeout(15000);//ms
+        return factory;
     }
 }
